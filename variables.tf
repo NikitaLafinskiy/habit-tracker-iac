aaf53b7ac9309802b899f0ed9619836a651bda2e
Nikita <niklafinskiy@gmail.com>
2026-07-11 13:46:56 +0200

    use local credentials instead of vars

--- variables.tf
+++ variables.tf
@@ -2,15 +2,3 @@ variable "aws_region" {
   type        = string
   description = "AWS region"
 }
-
-variable "access_key" {
-  type        = string
-  description = "AWS access key"
-  sensitive   = true
-}
-
-variable "secret_key" {
-  type        = string
-  description = "AWS secret key"
-  sensitive   = true
-}
