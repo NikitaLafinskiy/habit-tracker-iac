551ed76465a7424df2436315f491fef32e2eb964
Nikita <niklafinskiy@gmail.com>
2026-08-03 16:16:05 +0200

    sqs fix

--- modules/sqs/main.tf
+++ modules/sqs/main.tf
@@ -6,7 +6,7 @@ resource "aws_sqs_queue" "this" {
   receive_wait_time_seconds = var.receive_wait_time_seconds
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.this_deadletter.arn
-    maxReceiveCount     = var.maxReceiveCount
+    maxReceiveCount     = var.max_receive_count
   })
 
   tags = var.tags
@@ -18,11 +18,19 @@ resource "aws_sqs_queue" "this_deadletter" {
   tags = var.tags
 }
 
-resource "aws_sqs_queue_redrive_allow_policy" "this_redrive_allow_policy" {
+# Lives on the DLQ: which source queues may redrive into it. Valid
+# redrivePermission values are byQueue / allowAll / denyAll (not "deny").
+resource "aws_sqs_queue_redrive_allow_policy" "this" {
   queue_url = aws_sqs_queue.this_deadletter.id
 
-  redrive_allow_policy = jsonencode({
-    redrivePermission = var.redrivePermission,
-    sourceQueueArns   = [aws_sqs_queue.this.arn]
-  })
+  redrive_allow_policy = jsonencode(
+    var.redrive_permission == "byQueue"
+    ? {
+      redrivePermission = "byQueue"
+      sourceQueueArns   = [aws_sqs_queue.this.arn]
+    }
+    : {
+      redrivePermission = var.redrive_permission
+    }
+  )
 }
