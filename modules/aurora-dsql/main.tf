ce689a4f7158c7c3a816b68f97229d1af25560a6
Nikita <niklafinskiy@gmail.com>
2026-09-05 22:12:53 +0200

    aurora prevent destroy

--- modules/aurora-dsql/main.tf
+++ modules/aurora-dsql/main.tf
@@ -4,4 +4,8 @@ resource "aws_dsql_cluster" "this" {
   deletion_protection_enabled = var.deletion_protection_enabled
 
   tags = merge(var.tags, { Name = var.name })
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
